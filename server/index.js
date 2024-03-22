1123db86ba105dddfcb601833f23084335c9399b
Mayank Yadav <mayank4code@gmail.com>
2024-03-22 10:44:54 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const connectToMongo = require("./src/MongoDB/config");
+connectToMongo();
+
+// routes
+app.use("/api/user", require("./src/api/user"));
+app.use("/api/admin", require("./src/api/admin"));
+
+
+app.listen(5000, ()=>{
+    console.log("Server is running on port 5000");
+})
---
+++ server/src/MongoDB/config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const connectToMongo = ()=>{
+    mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("Connected to the database");
+    }).catch((err)=>{
+        console.log("Database connection failed: ", err);
+    })
+}
+
+module.exports = connectToMongo;
