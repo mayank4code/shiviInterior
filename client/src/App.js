a2f1cf7695841e487c4cc7fc67951f7b434ec5cf
Mayank Yadav <mayank4code@gmail.com>
2024-03-24 19:26:48 +0530

    trying to add Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 
+import Navbar from '../src/components/Navbar';
 
 import Login from './components/Login';
 import Register from '../src/components/Register';
@@ -12,6 +13,7 @@ import Home from '../src/components/Home';
 function App() {
   return (
     <Router className="App">
+      <Navbar/>
       <Routes>
         {/* User */}
           <Route path="/" element={<Home/>} />
